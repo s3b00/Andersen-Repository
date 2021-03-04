76e2b124234e067574921f72d280c24434f4e9b4
s3b00 <s3boo@yandex.by>
2021-03-04 21:37:55 +0300

    Updated stopping app

--- 2.js
+++ 2.js
@@ -1,8 +1,13 @@
 try {
-    const a = +prompt("Число А:")
-    const b = +prompt("Число B:")
+    const a = parseInt(prompt("Число А:"))
+    if (isNaN(a)) throw new Error()
+
+    const b = parseInt(prompt("Число B:"))
+    if (isNaN(b)) throw new Error()
     
-    console.log(`Ответ: ${a+b},${a/b}`)
+    console.log(`Ответ: ${a+b}, ${a/b}`)
 } catch {
     console.log("Некорректный ввод!")
 }
+
+
