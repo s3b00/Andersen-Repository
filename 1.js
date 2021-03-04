d292d91804a177dabdc6a6ed45eeeec4af338b7a
s3b00 <s3boo@yandex.by>
2021-03-04 21:40:53 +0300

    Final Version

--- 1.js
+++ 1.js
@@ -1,8 +1,11 @@
-const number = prompt("Введите число для конвертации в другую систему счисления:")
-const base = prompt("Система счисления:")
-
 try {
-    console.log(parseInt(number).toString(base))
-} catch {   
+    const number = parseInt(prompt("Введите число для конвертации в другую систему счисления:"))
+    const base =  parseInt(prompt("Система счисления:"))
+    
+    if (isNaN(number)) throw new Error()
+    if (isNaN(base)) throw new Error()
+
+    console.log((number).toString(base))
+} catch (error) {   
     console.log("Некорректный ввод!")
 }
